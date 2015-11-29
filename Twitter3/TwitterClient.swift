d8282cb70271fcdf556c3957989a848caf8b8812
Minh Dinh <dtminhpt@gmail.com>
2015-11-29 07:59:43 +0700

    User Persistence and Session Events

--- Twitter3/TwitterClient.swift
+++ Twitter3/TwitterClient.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 let twitterConsumerKey = "iPBG3irL4MneVEyqJJoGIfCZs"
 let twitterConsumerSecret = "Y9dKWWlh38H3KDT9836xxgVRB8DEKgMOBXg8h2k8uHyGYIj0Eq"
 let twitterBaseURL = NSURL(string: "https://api.twitter.com")
@@ -15,8 +16,9 @@ let twitterBaseURL = NSURL(string: "https://api.twitter.com")
 
 
 class TwitterClient: BDBOAuth1RequestOperationManager {
+    var loginCompletion: ((user: User?, error: NSError?) -> ())?
+    
     class var sharedInstance: TwitterClient {
-        
         struct Static {
             
             static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
@@ -26,4 +28,72 @@ class TwitterClient: BDBOAuth1RequestOperationManager {
         return Static.instance
         
     }
+    func loginWithCompletion(completion: (user: User?, error: NSError?) -> ()) {
+        loginCompletion = completion
+        
+        //Fetch request token & redirect to authorization page
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope: nil, success: { (requestToken: BDBOAuth1Credential!) -> Void in
+            
+            print("Got the request token")
+            let authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+            
+            
+            UIApplication.sharedApplication().openURL(authURL!)
+            
+            
+            }) { (error: NSError!) -> Void in
+                
+                print("Failed to get the request token")
+                self.loginCompletion?(user: nil, error: error)
+                
+        }
+        
+        
+    }
+    func homeTimeLineWithParams(params: NSDictionary?, completion: (tweets: [Tweet]?, error: NSError?) -> ()) {
+        GET("1.1/statuses/home_timeline.json", parameters: params, success: { (operation: AFHTTPRequestOperation, response: AnyObject) -> Void in
+            
+                //print("home timeline:\(response)")
+            
+                let tweets = Tweet.tweetsWithArray(response as! [NSDictionary])
+            
+                completion(tweets: tweets, error: nil)
+            
+            }, failure: { (operation: AFHTTPRequestOperation?, error: NSError) -> Void in
+                print("error getting home timeline")
+                    
+                completion(tweets: nil, error: error)
+         })
+    }
+    
+
+    
+    func openURL(url: NSURL) {
+        
+            fetchAccessTokenWithPath("oauth/access_token", method: "POST", requestToken: BDBOAuth1Credential(queryString: url.query) , success: { (accessToken: BDBOAuth1Credential!) -> Void in
+            print("Got the access token!")
+                
+            TwitterClient.sharedInstance.requestSerializer.saveAccessToken(accessToken)
+            
+            TwitterClient.sharedInstance.GET("1.1/account/verify_credentials.json", parameters: nil, success: { (operation: AFHTTPRequestOperation, response: AnyObject) -> Void in
+                //print("user:\(response)")//lay response
+                let user = User(dictionary: response as! NSDictionary)
+                print("user: \(user.name)")
+                User.currentUser = user
+                self.loginCompletion?(user: user, error: nil)
+                
+                }, failure: { (operation: AFHTTPRequestOperation?, error: NSError) -> Void in
+                    print("error getting current user")
+                    self.loginCompletion?(user: nil, error: error)
+            })
+            
+             
+            }) { (error: NSError!) -> Void in
+                print("Failed to receive access token")
+                self.loginCompletion?(user: nil, error: error)
+        }
+        
+    }
+
  }
---
+++ Twitter3/User.swift
@@ -0,0 +1,80 @@
+//
+//  User.swift
+//  Twitter3
+//
+//  Created by Dinh Thi Minh on 11/27/15.
+//  Copyright © 2015 Dinh Thi Minh. All rights reserved.
+//
+
+import UIKit
+
+var _currentUser: User?
+let currentUserKey = "kCurrentUserKey"
+let userDidLoginNotification = "userDidLoginNotification"
+let userDidLogoutNotification = "userDidLogoutNotification"
+
+class User: NSObject {
+    var name: String?
+    var screenname: String?
+    var profileImageURL: String?
+    var tagline: String?
+    var dictionary: NSDictionary
+    
+    init(dictionary: NSDictionary) {
+        self.dictionary = dictionary
+        
+        name = dictionary["name"] as? String
+        screenname = dictionary["screen_name"] as? String
+        profileImageURL = dictionary["profile_image_url"] as? String
+        tagline = dictionary["description"] as? String
+    }
+    
+    func logout() {
+        User.currentUser = nil
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        
+        NSNotificationCenter.defaultCenter().postNotificationName(userDidLogoutNotification, object: nil)    }
+    
+    class var currentUser: User? {
+        get {
+            if _currentUser == nil {
+                let data = NSUserDefaults.standardUserDefaults().objectForKey(currentUserKey) as? NSData
+                if data != nil {
+                    do{
+                        let dictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as! NSDictionary
+        
+                         _currentUser = User(dictionary: dictionary)
+                    } catch {
+                        print("json object with data error")
+                    }
+        
+        
+                }
+            }
+            return _currentUser
+        }
+        
+        set(user) {
+            _currentUser = user
+            
+            if _currentUser != nil {
+                do{
+                    
+                    let data = try NSJSONSerialization.dataWithJSONObject(user!.dictionary, options: [])
+                    NSUserDefaults.standardUserDefaults().setObject(data, forKey: currentUserKey)
+
+
+                } catch {
+                    print("json object with data error")
+                }
+              
+                    
+            } else {
+                NSUserDefaults.standardUserDefaults().setObject(nil, forKey: currentUserKey)
+            }
+            NSUserDefaults.standardUserDefaults().synchronize()            
+        }
+    }
+
+
+}
